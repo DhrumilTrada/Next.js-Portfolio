dd38a93f25284694984cf83be670396d40591c90
DhrumilTrada <dhrumiltrada11@gmail.com>
2025-03-19 16:43:37 +0530

    chore: fix some alignment issues and adding mongoDB, nestJs to toolkit

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -15,10 +15,10 @@ export const CardHeader = ({
       <div className="inline-flex items-center gap-2">
         <StarIcon className="size-9 text-emerald-300" />
         <h3 className="font-serif text-3xl">{title}</h3>
-        <p className="text-sm lg:text-base max-w-xs text-white/60 mt-2">
-          {description}
-        </p>
       </div>
+      <p className="text-sm lg:text-base max-w-xs text-white/60 mt-2">
+        {description}
+      </p>
     </div>
   );
 };
